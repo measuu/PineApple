c508596498bb8c7bc949c4d5a5dd4412baf1e77a
Dima <orlyk.do@gmail.com>
2026-02-03 18:24:54 +0100

    Add profile to the website

--- PineApple/urls.py
+++ PineApple/urls.py
@@ -20,4 +20,5 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('', TemplateView.as_view(template_name='core/home.html'), name='home'),
     path('', include('users.urls')),
+    path('accounts/', include('accounts.urls')),
 ]
