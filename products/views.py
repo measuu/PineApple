cef01281f73e2782ae9d4bdec40d0023549aae1e
Dima <orlyk.do@gmail.com>
2026-02-08 00:34:49 +0100

    Improve html and css for products

--- products/views.py
+++ products/views.py
@@ -1,9 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
+
 
 def category_products(request, category):
     products = Product.objects.filter(category=category)
     return render(request, 'products/category_products.html', {
         'products': products,
         'category': category
+    })
+
+def product_detail(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    return render(request, 'products/product_detail.html', {
+        'product': product
     })
