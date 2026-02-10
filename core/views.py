bd3de4e342883597a55b4c867159e3e56f037a40
Dima <orlyk.do@gmail.com>
2026-02-10 23:46:34 +0100

    Add search system in header, update html and css

--- core/views.py
+++ core/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from products.models import Product
 
-from django.shortcuts import render
 
-def home(request):
-    return render(request, 'core/home.html')
+def home_view(request):
+    query = request.GET.get("q", "").strip()
+
+    products = Product.objects.filter(name__icontains=query) if query else None
+    if products and products.count() == 1:
+        product = products.first()
+        return redirect('product_detail', product.id)
+
+    return render(request, "core/home.html", {"products": products})
