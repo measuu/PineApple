5c9503cf252b8c7b4d35973912ce258cc089a269
Dima <orlyk.do@gmail.com>
2026-02-11 18:23:13 +0100

    Try to update filters

--- products/filters.py
+++ products/filters.py
@@ -4,11 +4,11 @@ from .models import Product
 class ProductFilter(django_filters.FilterSet):
     min_price = django_filters.NumberFilter(field_name='price', lookup_expr='gte', label='Min Price')
     max_price = django_filters.NumberFilter(field_name='price', lookup_expr='lte', label='Max Price')
-    min_rating = django_filters.NumberFilter(method='filter_min_rating', label='Min Rating')
+    # min_rating = django_filters.NumberFilter(method='filter_min_rating', label='Min Rating')
 
     class Meta:
         model = Product
-        fields = []
+        fields = ["min_price", "max_price"]
 
     def filter_min_rating(self, queryset, name, value):
         return [p for p in queryset if p.average_rating() >= value]
